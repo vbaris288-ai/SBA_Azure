23a42edafd360513e8d445dc50e7e1955eebac4f
Student <student@wroclaw.merito.pl>
2026-06-14 09:23:59 +0200

    Version 1: Monolith frontend

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='.')
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
